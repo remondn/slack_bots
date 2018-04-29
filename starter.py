81a656fd74e2c5c87b22efa134cf3acf929979d5
Nicolas REMOND <remondnicola@gmail.com>
2018-04-29 11:45:59 +0900

    test

--- starter.py
+++ starter.py
@@ -28,9 +28,9 @@ def handle_msg(msg, channel):
     print("MATCH = |{}|{}|".format(match[0].upper().strip(), match[2].strip()))
     print("bool = {}".format(match[2].strip() is "do"))
 
-    if match[2].strip() is "do":
+    if match[2].strip() == "do":
         return ["You can do it !", channel]
-    elif match[0].upper().strip() is "HEY":
+    elif match[0].upper().strip() == "HEY":
         return ["Hey !", channel]        
     else:
         return ["Sorry, I'm a beginner, I know only one command : do", channel]
